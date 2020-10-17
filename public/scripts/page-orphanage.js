dcbe3b716316f8c036fac535f953da5249c1ec17
felipealvessi <felipealvesinfor@mail.com>
2020-10-17 16:48:46 -0300

    :rocket: initial commit

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,48 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
+
+// create map
+const map = L.map("mapid", options).setView([-22.5283723, -41.9410688], 15);
+// create and add tilelayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+// popup
+
+// create icon
+const icon = L.icon({
+  iconUrl: ".././images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// create and add marker
+const spanLat = document.querySelector("span[data-lat");
+const spanLng = document.querySelector("span[data-lng");
+
+L.marker([spanLat.dataset.lat, spanLng.dataset.lng], { icon }).addTo(map);
+
+// image galery
+
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  const buttons = document.querySelectorAll(".images button");
+
+  buttons.forEach(removeActiveClass);
+
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
+
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+
+  imageContainer.src = image.src;
+
+  button.classList.add("active");
+}
